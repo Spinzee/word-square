54329cbbbf1cd894ec00a80a40863790662869c3
SpinZee <Lindsey.Donaldson@live.com>
2026-01-28 20:25:23 +0000

    Add fix for Fly.io

--- WordSquare/Program.cs
+++ WordSquare/Program.cs
@@ -11,7 +11,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add this for Fly.io proxy support
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedFor
+        | Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.XForwardedProto;
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
+
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 app.MapOpenApi();
 app.UseSwaggerUI(options =>
@@ -19,8 +30,6 @@ app.UseSwaggerUI(options =>
     options.SwaggerEndpoint("/openapi/v1.json", "v1");
     options.RoutePrefix = string.Empty;
 });
-
-//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
